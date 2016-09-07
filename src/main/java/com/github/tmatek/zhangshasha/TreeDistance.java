7c158830bcd9687bc3d415c5dcb17c04104893c2
Tadej Matek <matektadej@gmail.com>
2016-09-07 19:00:57 +0200

    leftmost leaf descendant implementation

--- src/main/java/com/github/tmatek/zhangshasha/TreeDistance.java
+++ src/main/java/com/github/tmatek/zhangshasha/TreeDistance.java
@@ -1,6 +1,8 @@
 package com.github.tmatek.zhangshasha;
 
+import java.util.ArrayList;
 import java.util.IdentityHashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -41,5 +43,47 @@ public final class TreeDistance {
 
         map.put(current, idSequence.value++);
     }
+
+    /**
+     * Returns a list of leftmost leaf descendants (using postorder IDs) for every postorder ID node.
+     * @param root the root node to start the search from
+     * @param postorderIDs a mapping of tree nodes to postorder IDs
+     * @return a list of leftmost leaf descendants for every node
+     */
+    public static int[] leftmostLeafDescendants(TreeNode root, Map<TreeNode, Integer> postorderIDs) {
+        int[] lmld = new int[postorderIDs.get(root) + 1];
+        leftmostLeafDescendantsRec(root, lmld, new ArrayList<>(), postorderIDs);
+        return lmld;
+    }
+
+    /**
+     * Recursively find the leftmost leaf descendants for all nodes in the tree.
+     * @param current the current node being processed
+     * @param ref the list reference in which to store leftmost descendants
+     * @param chain the current path in the tree
+     * @param postorderIDs a mapping of tree nodes to postorder IDs
+     */
+    private static void leftmostLeafDescendantsRec(TreeNode current, int[] ref, List<TreeNode> chain,
+                                                   Map<TreeNode, Integer> postorderIDs) {
+
+
+        if (current.getChildren().size() == 0) {
+            // leftmost descendant of a leaf is the leaf itself
+            ref[postorderIDs.get(current)] = postorderIDs.get(current);
+
+            // assign the rest of nodes in the chain the same leftmost leaf descendant - this leaf
+            for (TreeNode ancestor : chain)
+                ref[postorderIDs.get(ancestor)] = postorderIDs.get(current);
+
+        } else {
+            chain.add(current);
+
+            int i = 0;
+            for (TreeNode child : current.getChildren())
+                leftmostLeafDescendantsRec(child, ref, i++ == 0 ? chain : new ArrayList<>(), postorderIDs);
+        }
+    }
+
+
 
 }
--- src/test/java/com/github/tmatek/zhangshasha/TreeDistanceTest.java
+++ src/test/java/com/github/tmatek/zhangshasha/TreeDistanceTest.java
@@ -15,6 +15,39 @@ public class TreeDistanceTest extends TestCase {
         StringTreeNode second = StringTreeNode.fromStringRepresentation("A(B(C))");
         postorderIds = TreeDistance.getPostorderIdentifiers(second);
         assertEquals(2, (int) postorderIds.get(second));
+
+        StringTreeNode root = new StringTreeNode("A");
+        root.addChild(new StringTreeNode("B"));
+        StringTreeNode c = new StringTreeNode("C");
+        root.addChild(c);
+        root.addChild(new StringTreeNode("D"));
+        c.addChild(new StringTreeNode("E"));
+        postorderIds = TreeDistance.getPostorderIdentifiers(root);
+        assertEquals(2, (int) postorderIds.get(c));
+    }
+
+    public void testLeftmostLeafDescendants() {
+        StringTreeNode a = new StringTreeNode("A");
+        StringTreeNode b = new StringTreeNode("B");
+        StringTreeNode c = new StringTreeNode("C");
+        StringTreeNode d = new StringTreeNode("D");
+        StringTreeNode e = new StringTreeNode("E");
+        StringTreeNode f = new StringTreeNode("F");
+
+        a.addChild(b);
+        b.addChild(c);
+        a.addChild(d);
+        a.addChild(e);
+        e.addChild(f);
+
+        Map<TreeNode, Integer> postorderIds = TreeDistance.getPostorderIdentifiers(a);
+        int[] lmld = TreeDistance.leftmostLeafDescendants(a, postorderIds);
+
+        assertEquals(0, lmld[5]);
+        assertEquals(2, lmld[2]);
+        assertEquals(0, lmld[1]);
+        assertEquals(3, lmld[3]);
+        assertEquals(3, lmld[4]);
     }
 
 }
